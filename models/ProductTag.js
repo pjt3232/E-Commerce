c5ba0df7dcc808ab513bca8e7eee83cc323dd226
Tranel <tranel@Tranels-MacBook-Air.local>
2023-05-27 12:57:00 -0500

    Added ProductTag & Tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
